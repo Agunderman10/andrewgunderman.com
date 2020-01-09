93223ba52c68afb3fe70a3f6ccab12ae143a4342
agunderman10 <agunderman10@gmail.com>
2020-01-09 06:01:09 -0500

    Minor changes

--- src/components/home-description.js
+++ src/components/home-description.js
@@ -12,10 +12,11 @@ function HomeDescription()
             <div className="container">
                 <div className="top-left">
                 <h1>𝘼 𝘽𝙊𝙔 𝙁𝙍𝙊𝙈 𝙉𝙊𝙒𝙃𝙀𝙍𝙀</h1>
-                    <p>Andrew Gunderman is a 19-year-old software developer, IT professional, podcast host, serial entrepreneur, and freshman at 
-                        Ohio State University who has been featured in articles all over the web for his achievements and renowned ambition. 
-                        Andrew is one of the founding fathers of Sprout, founder and CEO of Vyra, co-host of his podcast based on life and
-                        entrepreneurship, and software engineer performing contract work for several companies in the Ohio area.
+                    <p>Andrew Gunderman is a 19-year-old software developer, IT professional, podcast host, blogger, serial entrepreneur, 
+                        and freshman at Ohio State University who has been featured in articles all over the web for his achievements 
+                        and renowned ambition. Andrew is one of the founding fathers of Sprout, founder and CEO of Vyra, co-host of his 
+                        podcast based on life and entrepreneurship, and software engineer performing contract work for several companies 
+                        in the Ohio area.
                         <br/>
                         <br/>
                         Read My Story >>
